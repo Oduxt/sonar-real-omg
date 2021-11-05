9d5b762e360de417911bba6ee4f1f6bc433b3ec8
Oduxt <93281983+Oduxt@users.noreply.github.com>
2021-11-05 11:52:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ distance = 0
 def on_forever():
     global distance
     distance = sonar.ping(DigitalPin.P0, DigitalPin.P1, PingUnit.CENTIMETERS)
-    if distance <= 50:
+    if distance > 50:
         basic.show_leds("""
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
+            . . . . .
+                        . . . . .
+                        . . . . .
+                        . . . . .
+                        . . . . .
         """)
-    if distance <= 30:
+    if distance <= 50 and distance > 40:
         basic.show_leds("""
             . . . . .
                         . . . . .
@@ -19,7 +19,7 @@ def on_forever():
                         . . . . .
                         # # # # #
         """)
-    if distance <= 20:
+    if distance <= 40 and distance > 30:
         basic.show_leds("""
             . . . . .
                         . . . . .
@@ -27,23 +27,23 @@ def on_forever():
                         # # # # #
                         # # # # #
         """)
-    if distance <= 15:
+    if distance <= 30 and distance > 20:
         basic.show_leds("""
             . . . . .
                         . . . . .
+                        # # # # #
+                        # # # # #
+                        # # # # #
+        """)
+    if distance <= 20 and distance > 10:
+        basic.show_leds("""
+            . . . . .
+                        # # # # #
                         # # # # #
                         # # # # #
                         # # # # #
         """)
     if distance <= 10:
-        basic.show_leds("""
-            . . . . .
-                        # # # # #
-                        # # # # #
-                        # # # # #
-                        # # # # #
-        """)
-    if distance <= 5:
         basic.show_leds("""
             # # # # #
                         # # # # #
